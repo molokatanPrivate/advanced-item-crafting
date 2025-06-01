0d34c095b53c52de2fd1c9966592abd6fbaf68b9
molokatan <molokatan@live.de>
2025-06-01 05:04:11 +0200

    [feature] hightlight selected item

--- AdvancedItemCrafting.cs
+++ AdvancedItemCrafting.cs
@@ -302,6 +302,8 @@ namespace Oxide.Plugins
                 EpicBuffDescription(builder, player, baseItem.buff.Buff);
             }
 
+            HighlightSelectedItem(builder, baseItem);
+
             CuiHelper.AddUi(player, builder);
         }
 
@@ -394,6 +396,8 @@ namespace Oxide.Plugins
             
             AddEpicBuffDetailsPanel(builder);
             EpicBuffDescription(builder, player, baseItem.buff.Buff);
+
+            HighlightSelectedItem(builder, baseItem);
 
             CuiHelper.AddUi(player, builder);
         }
@@ -512,6 +516,8 @@ namespace Oxide.Plugins
                 var baseItem = new BaseItem(itemToMod);
                 CreateItemDetailsBase(builder, player, baseItem);
                 CreateItemActions(builder, player, baseItem);
+
+                HighlightSelectedItem(builder, baseItem);
             }
 
             CuiHelper.AddUi(player, builder);
@@ -544,6 +550,8 @@ namespace Oxide.Plugins
 
             CreateItemDetailsBase(builder, player, baseItem);
             CreateItemActions(builder, player, baseItem);
+            
+            HighlightSelectedItem(builder, baseItem);
 
             CuiHelper.AddUi(player, builder);
         }
@@ -579,6 +587,8 @@ namespace Oxide.Plugins
 
             CreateItemDetailsBase(builder, player, baseItem);
             CreateItemActions(builder, player, baseItem);
+            
+            HighlightSelectedItem(builder, baseItem);
 
             CuiHelper.AddUi(player, builder);
         }
@@ -1864,6 +1874,11 @@ namespace Oxide.Plugins
                 builder.AddItemButton(item, 50, $"{COMMAND_SELECT_ITEM} {item.uid.Value}", $"{INVENTORY_ITEM_SLOT}_{item.Slot}", $"{ITEM_WRAPPER}_{item.uid.Value}");
         }
 
+        public void HighlightSelectedItem(ExtendedCuiElementContainer builder, BaseItem item)
+        {
+            builder.Add(new CuiElement { Parent = $"{ITEM_WRAPPER}_{item.uid.Value}_BG", Name = "CURRENT_SELECTED_ITEM", Components = { new CuiRawImageComponent { Color = "0.08627 0.25490 0.38431 1", Sprite = "assets/content/ui/ui.background.tiletex.psd" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"0 0", OffsetMax = $"0 0" } }, DestroyUi = $"CURRENT_SELECTED_ITEM"});
+        }
+
         public void AddPlayerBuffsPanel(ExtendedCuiElementContainer builder, BasePlayer player, PlayerState playerState)
         {
             builder.Add(new CuiPanel { Image = { Color = "0 0 0 0.5" }, RectTransform = { AnchorMin = "0.5 0", AnchorMax = "0.5 0", OffsetMin = "0 0", OffsetMax = "0 0" } }, BACKDROP_PANEL, PLAYER_BUFFS_PANEL, PLAYER_BUFFS_PANEL);
@@ -2743,11 +2758,11 @@ namespace Oxide.Plugins
                 var imageSize = Size / 1.2f;
                 var imageOffset = (Size - imageSize) / 2f;
 
-                Add(new CuiPanel
-                {
-                    Image = { Color = "0 0 0 0" },
-                    RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = "0 0", OffsetMax = "0 0" },
-                }, Parent, Name, Name);
+                Add(new CuiPanel { Image = { Color = "0 0 0 0" }, RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = "0 0", OffsetMax = "0 0" } }, Parent, Name, Name);
+
+                Add(new CuiPanel { Image = { Color = "0 0 0 0" }, RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = "0 0", OffsetMax = "0 0" } }, Name, $"{Name}_BG");
+
+                Add(new CuiPanel { Image = { Color = "0 0 0 0" }, RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = "0 0", OffsetMax = "0 0" } }, Name, $"{Name}_FG");
 
                 // background for epic items
                 if (Item.buff != null && Instance.EpicLoot != null)
@@ -2755,61 +2770,24 @@ namespace Oxide.Plugins
                     var col = GetColorFromHtml(Instance.epicConfig.tier_information.tier_colours[Item.buff.Tier]);
                     var offset = imageOffset * 1.5f;
 
-                    Add(new CuiElement
-                    {
-                        Components = {
-                            new CuiRawImageComponent { Color = $"{col.r} {col.g} {col.b} {col.a}", Sprite = "assets/content/ui/tiledpatterns/swirl_pattern.png" },
-                            new CuiOutlineComponent { Color = "0.2641509 0.2641509 0.2641509 1", Distance = $"{offset / 1.5f} {-offset / 1.5f}" },
-                            new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{offset} {offset}", OffsetMax = $"{-offset} {-offset}" }
-                        },
-                        Parent = Name,
-                        Name = "EpicOutline",
-                    });
-                    Add(new CuiElement
-                    {
-                        Components = {
-                            new CuiRawImageComponent { Color = $"{col.r} {col.g} {col.b} 0.5", Sprite = "assets/content/ui/tiledpatterns/stripe_thin.png" },
-                            new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{offset} {offset}", OffsetMax = $"{-offset} {-offset}" }
-                        },
-                        Parent = Name,
-                        Name = "EpicBg",
-                    });
+                    Add(new CuiElement { Components = { new CuiRawImageComponent { Color = $"{col.r} {col.g} {col.b} {col.a}", Sprite = "assets/content/ui/tiledpatterns/swirl_pattern.png" }, new CuiOutlineComponent { Color = "0.2641509 0.2641509 0.2641509 1", Distance = $"{offset / 1.5f} {-offset / 1.5f}" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{offset} {offset}", OffsetMax = $"{-offset} {-offset}" } }, Parent = $"{Name}_FG", Name = "EpicOutline" });
+                    Add(new CuiElement { Components = { new CuiRawImageComponent { Color = $"{col.r} {col.g} {col.b} 0.5", Sprite = "assets/content/ui/tiledpatterns/stripe_thin.png" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{offset} {offset}", OffsetMax = $"{-offset} {-offset}" } }, Parent = $"{Name}_FG", Name = "EpicBg" });
                 }
 
-                Add(new CuiPanel {
-                    Image = { ItemId = Item.itemid, SkinId = Item.skin },
-                    RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{imageOffset} {imageOffset * 1.5}", OffsetMax = $"{-imageOffset} {-imageOffset * 0.5}" }
-                }, Name, "Icon");
+                Add(new CuiPanel { Image = { ItemId = Item.itemid, SkinId = Item.skin }, RectTransform = { AnchorMin = "0 0", AnchorMax = "1 1", OffsetMin = $"{imageOffset} {imageOffset * 1.5}", OffsetMax = $"{-imageOffset} {-imageOffset * 0.5}" } }, $"{Name}_FG", "Icon");
 
                 if (Item.perks.Count > 0 && Instance.ItemPerks != null)
                 {
-                    Add(new CuiElement
-                    {
-                        Components = {
-                            new CuiRawImageComponent { Color = "0.2641509 0.2641509 0.2641509 1", Sprite = "assets/icons/star.png" },
-                            new CuiRectTransformComponent { AnchorMin = "1 0", AnchorMax = "1 0", OffsetMin = $"-24 4", OffsetMax = $"-4 24" }
-                        },
-                        Parent = Name,
-                        Name = "PerkIcon",
-                    });
+                    Add(new CuiElement { Components = { new CuiRawImageComponent { Color = "0.2641509 0.2641509 0.2641509 1", Sprite = "assets/icons/star.png" }, new CuiRectTransformComponent { AnchorMin = "1 0", AnchorMax = "1 0", OffsetMin = $"-24 4", OffsetMax = $"-4 24" } }, Parent = $"{Name}_FG", Name = "PerkIcon" });
 
-                    Add(new CuiElement
-                    {
-                        Components = {
-                            new CuiTextComponent { Text = $"{Item.perks.Count}", Font = "robotocondensed-bold.ttf", FontSize = 14, Align = TextAnchor.MiddleCenter, Color = "1 1 1 1" },
-                            new CuiOutlineComponent { Color = "0 0 0 1", Distance = "1 -1" },
-                            new CuiRectTransformComponent { AnchorMin = "0.5 0.5", AnchorMax = "0.5 0.5", OffsetMin = "-10 -10", OffsetMax = "10 10" }
-                        },
-                        Parent = "PerkIcon",
-                        Name = "PerkLabel",
-                    });
+                    Add(new CuiElement { Components = { new CuiTextComponent { Text = $"{Item.perks.Count}", Font = "robotocondensed-bold.ttf", FontSize = 14, Align = TextAnchor.MiddleCenter, Color = "1 1 1 1" }, new CuiOutlineComponent { Color = "0 0 0 1", Distance = "1 -1" }, new CuiRectTransformComponent { AnchorMin = "0.5 0.5", AnchorMax = "0.5 0.5", OffsetMin = "-10 -10", OffsetMax = "10 10" } }, Parent = "PerkIcon", Name = "PerkLabel" });
 
                     if (!Item.named) return;
 
                     if (Item.restricted || !Instance.config.craft_settings.unveil_perk_settings.enabled || Item.perks.Count >= Instance.config.craft_settings.add_perk_settings.maxPossiblePerks)
-                        Add(new CuiElement { Components = { new CuiImageComponent { Color = "0.9 0.9 0.9 1", Sprite = "assets/icons/bp-lock.png" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "0 0", OffsetMin = $"6 6", OffsetMax = $"24 24" } }, Parent = Name, Name = "NamedPerkIcon" });
+                        Add(new CuiElement { Components = { new CuiImageComponent { Color = "0.9 0.9 0.9 1", Sprite = "assets/icons/bp-lock.png" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "0 0", OffsetMin = $"6 6", OffsetMax = $"24 24" } }, Parent = $"{Name}_FG", Name = "NamedPerkIcon" });
                     else
-                        Add(new CuiElement { Components = { new CuiImageComponent { Color = "0.9 0.9 0.9 1", Sprite = "assets/icons/examine.png" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "0 0", OffsetMin = $"6 4", OffsetMax = $"26 24" } }, Parent = Name, Name = "NamedPerkIcon" });
+                        Add(new CuiElement { Components = { new CuiImageComponent { Color = "0.9 0.9 0.9 1", Sprite = "assets/icons/examine.png" }, new CuiRectTransformComponent { AnchorMin = "0 0", AnchorMax = "0 0", OffsetMin = $"6 4", OffsetMax = $"26 24" } }, Parent = $"{Name}_FG", Name = "NamedPerkIcon" });
 
                 }
             }
